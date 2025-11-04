4627c059adc0e905ed787327db1832d1cd00c57d
HappyLeo7 <suh0007@naver.com>
2025-11-04 17:39:11 +0900

    feat: service 패키지 추가 , MemService,MemServiceImpl 추가

---
+++ OneTouch/src/main/java/com/onetouch/service/MemService.java
@@ -0,0 +1,7 @@
+package com.onetouch.service;
+
+import com.onetouch.vo.MemVo;
+
+public interface MemService {
+	public MemVo idCheck();
+}
---
+++ OneTouch/src/main/java/com/onetouch/service/MemServiceImpl.java
@@ -0,0 +1,16 @@
+package com.onetouch.service;
+
+import org.springframework.stereotype.Service;
+
+import com.onetouch.vo.MemVo;
+
+@Service
+public class MemServiceImpl implements MemService {
+
+	@Override
+	public MemVo idCheck() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
