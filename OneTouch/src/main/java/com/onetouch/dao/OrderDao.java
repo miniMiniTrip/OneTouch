eb7c7c726d0ac30cebec8fa7c3b8c7c80950a671
quaver0405 <jannerdyo@gmail.com>
2025-11-13 08:08:45 +0900

    ref : order/ orderItem

--- OneTouch/src/main/java/com/onetouch/dao/OrderDao.java
+++ OneTouch/src/main/java/com/onetouch/dao/OrderDao.java
@@ -1,5 +1,7 @@
 package com.onetouch.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import com.onetouch.vo.OrderVo;
@@ -7,5 +9,10 @@ import com.onetouch.vo.OrderVo;
 //주문서 작성
 @Mapper
 public interface OrderDao {
+	int insert(OrderVo vo);
 	OrderVo insertOrder(OrderVo vo);
+	List<OrderVo> selectList(int mem_idx);
+	int updateStatus(int order_id, String order_status);
+	int update(OrderVo vo);
+	OrderVo selectOneByOrderNo(String order_no);
 }
--- OneTouch/src/main/java/com/onetouch/dao/OrderItemDao.java
+++ OneTouch/src/main/java/com/onetouch/dao/OrderItemDao.java
@@ -1,8 +1,12 @@
 package com.onetouch.dao;
 
+import java.util.List;
+
 import com.onetouch.vo.OrderItemVo;
 
 public interface OrderItemDao {
-	//결제내역 작성
-	OrderItemVo insertOrderItem(OrderItemVo vo);
+	int insert(OrderItemVo vo);
+	int insertProduct(List<OrderItemVo> list);
+	List<OrderItemVo> selectListByOrderId(int order_id);
+	OrderItemVo selectOne(int order_item_id);
 }
