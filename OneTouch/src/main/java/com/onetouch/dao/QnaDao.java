b667071c4eebf28894f3f26b89751352b59ac141
R1oN91 <rhdbqls002@naver.com>
2025-11-12 18:24:36 +0900

    feat : (YB)  // 3️⃣ 답변 등록 (추가)

--- OneTouch/src/main/java/com/onetouch/dao/QnaDao.java
+++ OneTouch/src/main/java/com/onetouch/dao/QnaDao.java
@@ -1,6 +1,7 @@
 package com.onetouch.dao;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 
@@ -10,7 +11,7 @@ import com.onetouch.vo.QnaVo;
 public interface QnaDao {
 
     // 1️⃣ 전체 문의글 목록
-    List<QnaVo> selectQnaList(int mem_idx);
+    List<QnaVo> selectQnaList(Map<String, Object> mem_idx);
 
     // 2️⃣ 특정 문의글 상세보기
     QnaVo selectQnaDetail(int qna_idx);
@@ -24,6 +25,7 @@ public interface QnaDao {
      int updateQna(QnaVo vo);
 
 	 int deleteQna(int qna_idx);
-    }
 
-
+	 // 3️⃣ 답변 등록 (추가)
+	 int updateAnswer(Map<String, Object> map);
+	 }
