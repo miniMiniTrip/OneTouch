641a9a8ea2c2ab92687fc50da8407b10a27b4e99
quaver0405 <jannerdyo@gmail.com>
2025-11-17 18:16:36 +0900

    feat : payment(Dao, Service, xml)

---
+++ OneTouch/src/main/java/com/onetouch/dao/PaymentDao.java
@@ -0,0 +1,17 @@
+package com.onetouch.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.onetouch.vo.PaymentVo;
+
+@Mapper
+public interface PaymentDao {
+	int insertPayment(PaymentVo vo);
+	
+	PaymentVo selectPaymentByKey(String payment_key);
+	PaymentVo selectPaymentByOrderId(int order_id);
+	
+	int updatePaymentApproved(PaymentVo vo);
+	int updatePaymentFailed(PaymentVo vo);
+	int updatePaymentCanceled(PaymentVo vo);	
+}
---
+++ OneTouch/src/main/java/com/onetouch/service/PaymentService.java
@@ -0,0 +1,15 @@
+package com.onetouch.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.onetouch.dao.PaymentDao;
+
+@Service
+public class PaymentService {
+	
+	@Autowired
+	PaymentDao payment_dao;
+	
+}
