c0e6ce398e79435f8e128766b66497f87e3fa236
R1oN91 <rhdbqls002@naver.com>
2025-11-18 14:37:00 +0900

    스페이스바 공간

--- OneTouch/src/main/java/com/onetouch/controller/AdminCotroller.java
+++ OneTouch/src/main/java/com/onetouch/controller/AdminCotroller.java
@@ -3,8 +3,9 @@ package com.onetouch.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-//@Controller
-//public class AdminCotroller {
+@Controller
+//@RequestMapping("/adminpage")
+public class AdminCotroller {  // 클래스명 수정 (Cotroller → Controller)
 	
 //	//관리자페이지 열기
 //	@RequestMapping("/adminpage")
