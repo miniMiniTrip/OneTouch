fb14e50571b8b662c38130b711af5bfaf742a7b6
quaver0405 <jannerdyo@gmail.com>
2025-11-20 08:09:29 +0900

    feat: paymentSercice.java

--- OneTouch/src/main/java/com/onetouch/service/PaymentService.java
+++ OneTouch/src/main/java/com/onetouch/service/PaymentService.java
@@ -2,8 +2,10 @@ package com.onetouch.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.onetouch.dao.PaymentDao;
+import com.onetouch.vo.PaymentVo;
 
 @Service
 public class PaymentService {
@@ -11,6 +13,33 @@ public class PaymentService {
 	@Autowired
 	PaymentDao payment_dao;
 	
+	@Transactional
+	    public int createPaymentReady(PaymentVo vo) {
+	        vo.setStatus("READY");
+	    return payment_dao.insertPayment(vo);
+	}
+	 
+	@Transactional
+	public int approvePayment(PaymentVo vo) {
+	    return payment_dao.updatePaymentApproved(vo);
+	}	
 	
+	@Transactional
+	public int failPayment(PaymentVo vo) {
+	    return payment_dao.updatePaymentFailed(vo);
+	}
 	
+	@Transactional
+	public int cancelPayment(PaymentVo vo) {
+	    return payment_dao.updatePaymentCanceled(vo);
+	}
+	
+	public PaymentVo getPaymentByKey(String payment_key) {
+	    return payment_dao.selectPaymentByKey(payment_key);
+	}
+	
+	public PaymentVo getPaymentByOrderId(int order_id) {
+	    return payment_dao.selectPaymentByOrderId(order_id);
+	}
+		 
 }
