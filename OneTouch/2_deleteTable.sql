8d9ec7ca5bcc8036b43f86b05615582935002491
HappyLeo7 <suh0007@naver.com>
2025-12-02 10:52:19 +0900

    chore : 드랍 테이블,  딜리트 테이블 sql 분리

--- OneTouch/2_deleteTable.sql
+++ OneTouch/2_deleteTable.sql
@@ -1,27 +1,3 @@
--- 모든 테이블 삭제
-drop table wishlist;
-drop table skin_profile;
-drop table reply;
-drop table qna;
--- drop table product_remain; -- 안쓸 테이블
--- drop table product_out; -- 안쓸 테이블
--- drop table product_in;
-drop table product_image;
-drop table product_hashtag;
-drop table cart;
-drop table post_product;
-drop table product;
-drop table post_hashtag;
-drop table `like`;
-drop table post;
-drop table payment;
-drop table order_item;
-drop table `order`;
-drop table mem;
-drop table hashtag;
-drop table category;
-
-
 
 -- 모든 테이블 데이터 삭제
 
