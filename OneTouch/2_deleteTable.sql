283b0b586cffe46fa2d42136adbbd41273ff2cf3
HappyLeo7 <suh0007@naver.com>
2025-12-02 10:50:29 +0900

    chore : sql 통해서 모든 db 내용 삭제와 테이블 생성 , 더미 데이터 넣을수 있게 수정

---
+++ OneTouch/2_deleteTable.sql
@@ -0,0 +1,84 @@
+-- 모든 테이블 삭제
+drop table wishlist;
+drop table skin_profile;
+drop table reply;
+drop table qna;
+-- drop table product_remain; -- 안쓸 테이블
+-- drop table product_out; -- 안쓸 테이블
+-- drop table product_in;
+drop table product_image;
+drop table product_hashtag;
+drop table cart;
+drop table post_product;
+drop table product;
+drop table post_hashtag;
+drop table `like`;
+drop table post;
+drop table payment;
+drop table order_item;
+drop table `order`;
+drop table mem;
+drop table hashtag;
+drop table category;
+
+
+
+-- 모든 테이블 데이터 삭제
+
+SET FOREIGN_KEY_CHECKS = 0;
+
+DELETE FROM reply;
+ALTER TABLE reply AUTO_INCREMENT = 1;
+
+DELETE FROM qna;
+ALTER TABLE qna AUTO_INCREMENT = 1;
+
+DELETE FROM wishlist;
+ALTER TABLE wishlist AUTO_INCREMENT = 1;
+
+DELETE FROM skin_profile;
+ALTER TABLE skin_profile AUTO_INCREMENT = 1;
+
+DELETE FROM cart;
+ALTER TABLE cart AUTO_INCREMENT = 1;
+
+DELETE FROM post_hashtag;
+ALTER TABLE post_hashtag AUTO_INCREMENT = 1;
+
+DELETE FROM post_product;
+ALTER TABLE post_product AUTO_INCREMENT = 1;
+
+DELETE FROM product_image;
+ALTER TABLE product_image AUTO_INCREMENT = 1;
+
+DELETE FROM product_hashtag;
+ALTER TABLE product_hashtag AUTO_INCREMENT = 1;
+
+DELETE FROM `like`;
+ALTER TABLE `like` AUTO_INCREMENT = 1;
+
+DELETE FROM order_item;
+ALTER TABLE order_item AUTO_INCREMENT = 1;
+
+DELETE FROM payment;
+ALTER TABLE payment AUTO_INCREMENT = 1;
+
+DELETE FROM post;
+ALTER TABLE post AUTO_INCREMENT = 1;
+
+DELETE FROM product;
+ALTER TABLE product AUTO_INCREMENT = 1;
+
+DELETE FROM `order`;
+ALTER TABLE `order` AUTO_INCREMENT = 1;
+
+DELETE FROM mem;
+ALTER TABLE mem AUTO_INCREMENT = 1;
+
+DELETE FROM hashtag;
+ALTER TABLE hashtag AUTO_INCREMENT = 1;
+
+DELETE FROM category;
+ALTER TABLE category AUTO_INCREMENT = 1;
+
+SET FOREIGN_KEY_CHECKS = 1;
